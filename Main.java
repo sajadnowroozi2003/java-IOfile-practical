57134909b604a1f2d799601d89f19216517aae22
Sajad Nowroozi <0799111111aminy11@gmail.com>
2026-01-11 22:36:57 +0430

    I used form Scanner to Read Student.csv file and its take 31 ms to read this file

--- Main.java
+++ Main.java
@@ -1,4 +1,36 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Scanner;
+
+import javax.annotation.processing.Filer;
+
 class Main{
     public static void main(String[] args) {
+        if (args.length==0) {
+            System.out.println("No File Exist.");
+            return;
+        }
+      Instant start =  Instant.now(); 
+try(Scanner scan = new Scanner(new FileReader(args[0]))){
+
+    while (scan.hasNextLine()) {
+          
+String line =scan.nextLine();
+System.out.println(line);
+    }
+
+
+
+}catch(IOException e){
+    System.out.println(e);
+}
+  Instant end = Instant.now(); 
+  
+  Duration duration =  Duration.between(start, end);
+  System.out.println(duration.toMillis()+" ms");
+
     }
 }
